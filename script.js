260771f2d0c35e42d57203c045299a721d352f09
shahbajjamil <shahbajjamil2014@gmail.com>
2020-09-20 16:51:34 +0530

    letGo

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menuToggle = document.querySelector(".toggle");
+  // var banner = document.querySelector(".banner");
+  var menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+  menuToggle.classList.toggle("active");
+  // banner.classList.toggle("active");
+}
