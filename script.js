537b730716fb79b3a93ea8d3fefdba888c3b9ee9
shahbajjamil <shahbajjamil2014@gmail.com>
2020-09-22 16:43:21 +0530

    gallery with open image

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ function toggleMenu() {
   menuToggle.classList.toggle("active");
   // banner.classList.toggle("active");
 }
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".thumbnail");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
+
+previews.forEach((preview) => {
+  preview.addEventListener("click", () => {
+    modal.classList.add("open");
+    original.classList.add("open");
+    // Dynamic change text and image
+    const originalScr = preview.getAttribute("data-original");
+    console.log(originalScr);
+    original.src = `images/gallery/${originalScr}`;
+    const altText = preview.alt;
+    caption.textContent = altText;
+  });
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
