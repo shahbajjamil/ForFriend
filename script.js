6940eafa43c7ae231b74ddb0073b449b6fec0772
shahbajjamil <shahbajjamil2014@gmail.com>
2020-10-05 01:05:35 +0530

    menu enable full function for both pc and mobile

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const menuToggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const menuAll = document.querySelectorAll(".menu ul li a");
+const home = document.getElementById("homeNav");
+const about = document.getElementById("aboutNav");
+const skill = document.getElementById("skillNav");
+const gallery  = document.getElementById("galleryNav");
+const contact= document.getElementById("contactNav");
 
 menuToggle.addEventListener("click", toggleMenu);
 menuAll.forEach((all) => {
   all.addEventListener("click", function () {
-    console.log(all.innerHTML);
+    // console.log(all.innerHTML);
     toggleMenu();
     menuAll.forEach((all) => all.classList.remove("active"));
     all.classList.add("active");
@@ -15,6 +20,59 @@ menuAll.forEach((all) => {
 function toggleMenu() {
   menu.classList.toggle("active");
   menuToggle.classList.toggle("active");
+}
+
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(
+  navigator.userAgent
+);
+
+window.onscroll = function () {
+  var positionTop = document.documentElement.scrollTop;
+  if (isMobile){
+    // console.log("mobile "+positionTop);
+    if (positionTop> 130) {
+      menuToggle.style.top = 10+'px';
+      menu.style.top = 70+'px';
+    } else {
+      menuToggle.style.top = 100+'px';
+      menu.style.top = 160+'px';
+    }
+    if(positionTop > 6578){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      contactNav.classList.add("active");
+    } else  if(positionTop > 3910){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      galleryNav.classList.add("active");
+    } else  if(positionTop > 2970){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      skillNav.classList.add("active");
+    } else  if(positionTop > 683){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      aboutNav.classList.add("active");
+    } else  if(positionTop >= 0){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      homeNav.classList.add("active");
+    }
+  } 
+  else {
+    // console.log("other "+positionTop);
+    if(positionTop > 2940){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      contactNav.classList.add("active");
+    } else  if(positionTop > 2148){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      galleryNav.classList.add("active");
+    } else  if(positionTop > 1600){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      skillNav.classList.add("active");
+    } else  if(positionTop > 535){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      aboutNav.classList.add("active");
+    } else  if(positionTop >= 0){
+      menuAll.forEach((all) => all.classList.remove("active"));
+      homeNav.classList.add("active");
+    }
+  }
 }
 
 const lightBoxContainer = document.querySelector(".lightbox");
